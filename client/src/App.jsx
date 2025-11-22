cd2fcec6209a854a301975aecf8f01d72482d4c5
Sujal <vend-sujal@spotdraft.com>
2025-11-23 00:09:54 +0530

    Basic project setup client and server

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="text-3xl font-bold ">
+      <h1 className="text-5xl text-pink-400 text-center">What's the best?</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from "express";  
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.json());
+
+
+app.get("/", (req, res)=>{
+    res.json({message: "Health check fine!"})
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
+})
